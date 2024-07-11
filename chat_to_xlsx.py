8361438a8ee615acefccd02877f57971fa778243
afhelper <heartyou.helper@gmail.com>
2024-07-12 06:35:41 +0900

    타임라인 - 3

--- chat_to_xlsx.py
+++ chat_to_xlsx.py
@@ -42,7 +42,7 @@ def sanitize_string(value):
 
 
 def format_timestamp(total_seconds):
-    total_seconds = round(total_seconds)
+    total_seconds = round(total_seconds) - 3
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
     seconds = total_seconds % 60
