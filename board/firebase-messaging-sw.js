fc2fb0c687eb8d80b81b7a208a61794d97015c39
afhelper <heartyou.helper@gmail.com>
2024-07-30 22:12:23 +0900

    -

--- board/firebase-messaging-sw.js
+++ board/firebase-messaging-sw.js
@@ -1,8 +1,6 @@
-// Import scripts
-importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js');
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBeIPr1H_de7eIZUagNAUvPbw-rYRteP9U",
     authDomain: "submit-33eb1.firebaseapp.com",
@@ -12,20 +10,34 @@ const firebaseConfig = {
     appId: "1:123176179541:web:c40f2392c8b95fb93601dc"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-// Handle background messages
-messaging.onBackgroundMessage((payload) => {
+self.addEventListener('push', function (event) {
+    console.log('Push event received:', event);
+});
+
+messaging.onBackgroundMessage(function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
+
+    // 알림 표시 로직
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/firebase-logo.png'
+        icon: '/firebase-logo.png'  // 적절한 아이콘 경로로 변경하세요
     };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('[firebase-messaging-sw.js] Notification click Received.');
+
+    event.notification.close();
+
+    // 알림 클릭 시 특정 URL로 이동
+    event.waitUntil(
+        clients.openWindow('https://your-site-url.com')  // 원하는 URL로 변경하세요
+    );
 });
